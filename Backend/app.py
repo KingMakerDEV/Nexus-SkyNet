e549a4457aef8b5034a150d1ac5ba5ef5f922475
Abhay9326397439 <abhayzombade26@gmail.com>
2026-02-10 18:39:41 +0530

    Save local changes before merge

---
+++ Backend/app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_cors import CORS
+
+from app.extionsions import db
+from app.config import Config
+from Api_http_level.auth_routes import auth_bp
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    CORS(app, supports_credentials=True)
+
+    app.register_blueprint(auth_bp, url_prefix="/auth")
+
+    return app
+
+
+app = create_app()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
