55b23cbcab3307249b63f358f91caf07a3f40413
KingMakerDEV <wnikhil146@gmail.com>
2026-02-11 20:37:49 +0530

    fixing main issue git related

--- Backend/Services/auth_service.py
+++ Backend/Services/auth_service.py
@@ -1,5 +1,6 @@
 from repository.user_repo import get_user_by_email, create_user
 from werkzeug.security import generate_password_hash, check_password_hash
+from utils.jwt_helper import create_jwt_token
 
 
 def register_user(name, email, password):
@@ -7,16 +8,18 @@ def register_user(name, email, password):
         return {"error": "Email already exists"}, 400
 
     hashed = generate_password_hash(password)
-
     user = create_user(name, email, hashed)
+
+    token = create_jwt_token(user.id, remember=False)
 
     return {
         "message": "Registered successfully",
+        "token": token,
         "user_id": user.id
     }, 201
 
 
-def login_user(email, password):
+def login_user(email, password, remember=False):
     user = get_user_by_email(email)
 
     if not user:
@@ -25,7 +28,10 @@ def login_user(email, password):
     if not check_password_hash(user.password, password):
         return {"error": "Wrong password"}, 401
 
+    token = create_jwt_token(user.id, remember)
+
     return {
         "message": "Login success",
+        "token": token,
         "user_id": user.id
     }, 200
